8b595687805aeb720a1fbd43e6e640fa64ec4e4a
Chu-Jiwoo <smpts00@naver.com>
2022-06-17 00:07:40 +0900

    ㄸㅇ

--- src/routes/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var register_ctrl_1 = require("../controllers/register/register.ctrl");
-var router = express_1["default"].Router();
-// 스케쥴 관련 요청을 scrouter로 이동
-router.post('/register', register_ctrl_1["default"].PostRegister);
-exports["default"] = router;
