d35e3ea389f7d4a041d087e61a81fb990f4a9b32
slbin-park <smpts00@naver.com>
2023-03-09 19:42:34 +0900

    feat: winston 설정 추가

--- src/utils/winston.ts
+++ src/utils/winston.ts
@@ -1,7 +1,43 @@
 import { createLogger, transports, format } from "winston";
-
+import DailyRotateFile from "winston-daily-rotate-file";
 interface TransformableInfo {
   level: string;
   message: string;
   [key: string]: any;
 }
+
+const env = process.env.NODE_ENV || "development";
+const logDir = "log";
+
+const dailyRotateFileTransport = new transports.DailyRotateFile({
+  level: "debug",
+  filename: `${logDir}/%DATE%-smart-push.log`,
+  datePattern: "YYYY-MM-DD",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
+
+const logger = createLogger({
+  level: env === "development" ? "debug" : "info",
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    format.json()
+  ),
+  transports: [
+    new transports.Console({
+      level: "info",
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      ),
+    }),
+    dailyRotateFileTransport,
+  ],
+});
+
+export default logger;
